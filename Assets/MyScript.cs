25dbdaa121b5b2704031e32fd75a81aec3bd569a
Michele <michelelecca0@gmail.com>
2018-11-07 00:11:49 +0100

    Added Script

---
+++ Assets/MyScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("I'm trying to make this work I've already wasted 2 hours of my life NEVER GIVE UP!!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
