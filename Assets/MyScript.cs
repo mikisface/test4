84bbfa1df4a027dedcec75e762c8d78373c2e111
Michele <michelelecca0@gmail.com>
2018-11-07 00:13:55 +0100

    Something changed

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -7,6 +7,7 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("I'm trying to make this work I've already wasted 2 hours of my life NEVER GIVE UP!!!");
+		print ("Changed");
 	}
 	
 	// Update is called once per frame
